bde73cf8175589712c582cd7237c9ee00fab6f4f
OnuSike <117122235+OnuSike@users.noreply.github.com>
2024-05-10 20:27:09 +0300

    Update pb 2.c

--- pb 2.c
+++ pb 2.c
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <dirent.h>
 #include <time.h>
 
 #define BUFSIZE 4096
@@ -31,129 +32,226 @@ struct stat
 
 */
 
-void rezolvare(int i, char *input, char *output)
+long int _log10(long int x)
+{
+  int digits=0;
+  
+  if (x == 0)
+    return 1;
+  while (x != 0)
+    {
+      digits++;
+      x /= 10;
+    }
+  return digits;
+}
+
+void rezolvare(char *input, char *path, char *output_path)
 {
   // printf("rezolvare: %d: %s\n", i, input);
   
   int fd1, fd2, inod_digits=0, size_digits=0, mtime_digits=0;
-  long int aux;
-  char buf[BUFSIZE], *human_readable_date;
-  struct stat filedata;
+  char buf[BUFSIZE], *human_readable_date, virtual_path[256], inod_tostring[256], output_path2[256];
+  struct stat filedata, dirdata;
+
+  strcpy(virtual_path, path);
+  strcat(virtual_path, "/");
+  strcat(virtual_path, input);
   
-  if( (fd1 = open(input, O_RDONLY)) < 0)
+  if( (fd1 = open(virtual_path, O_RDONLY)) < 0)
     {
-      printf("Error opening input file\n");
-      exit(2);
+      stat(virtual_path, &filedata);
+      printf("Permission bits: %03o (%s)\n", filedata.st_mode&511, input);
+      if ((filedata.st_mode&511) == 0)
+	{
+	  printf("File with no permissions: %s\n\n", input);
+	  // TODO create process...
+	  return;
+	}
+      else
+	{
+	  printf("File does not exists in the current context\n");
+	  exit(3);
+	}
     }
 
   fstat(fd1, &filedata);
-  if( (fd2 = open(output, O_WRONLY | O_CREAT | O_APPEND, S_IRWXU)) < 0)
-    {
-      printf("Error creating destination file\n");
-      exit(3);
-    }
-
-  aux = filedata.st_ino;
-  while (aux != 0)
-    {
-      inod_digits++;
-      aux /= 10;
-    }
-
-  aux = filedata.st_size;
-  if (aux == 0)
-    size_digits++;
-  while (aux != 0)
-    {
-      size_digits++;
-      aux /= 10;
-    }
-
-  aux = filedata.st_mtime;
-  while (aux != 0)
-    {
-      mtime_digits++;
-      aux /= 10;
-    }
-  // write(fd2, buf, strlen(input)+8+digits);
-
-
   
-  sprintf(buf, "   INOD %ld\n\n", filedata.st_ino);
-  printf("   INOD %ld\n\n", filedata.st_ino);
-  write(fd2, buf, 10+inod_digits);
-
-  sprintf(buf, "File Name: %s\na", input);
-  printf("File Name: %s\n", input);
-  write(fd2, buf, 12+strlen(input));
+  inod_digits = _log10(filedata.st_ino);
+  size_digits = _log10(filedata.st_size);
+  mtime_digits = _log10(filedata.st_mtime);
 
   human_readable_date = ctime(&filedata.st_mtime);
   human_readable_date[strlen(human_readable_date)-1] = 0;
-  sprintf(buf, "Last Modification: %s (st_mtime: %ld)\n", human_readable_date, filedata.st_mtime);
-  printf("Last Modification: %s (st_mtime: %ld)\n", human_readable_date, filedata.st_mtime);
-  write(fd2, buf, 32+strlen(ctime(&filedata.st_mtime))+mtime_digits);
+// sprintf(buf, "   INOD %ld\n\nFile Name: %s\nLast Modification: %s (st_mtime: %ld)\nFile Size: %ld bytes\n\n\n", filedata.st_ino, input, human_readable_date, filedata.st_mtime, filedata.st_size);
+  sprintf(buf, "   INOD %ld\n\nFile Name: %s\nFull Path: %s\nLast Modification: %s (st_mtime: %ld)\nPermission bits: %03o\nFile Size: %ld bytes\n\n\n", filedata.st_ino, input, virtual_path, human_readable_date, filedata.st_mtime, filedata.st_mode&511, filedata.st_size);
+  // printf("%s\n", buf);
 
-  sprintf(buf, "File Size: %ld bytes\n\n\n", filedata.st_size);
-  printf("File Size: %ld bytes\n\n\n", filedata.st_size);
-  write(fd2, buf, 20+size_digits);
+  sprintf(inod_tostring, "%ld", filedata.st_ino);
+  strcpy(output_path2, output_path);
+  strcat(output_path2, "/");
+  strcat(output_path2, inod_tostring);
+  strcat(output_path2, ".txt");
   
-
-  /*
-  human_readable_date = ctime(&filedata.st_mtime);
-  human_readable_date[strlen(human_readable_date)-1] = 0;
- 
-  sprintf(buf, "   INOD %ld\n\nFile Name: %s\nLast Modification: %s (st_mtime: %ld)\nFile Size: %ld bytes\n\n\n", filedata.st_ino, input, human_readable_date, filedata.st_mtime, filedata.st_size);
-  printf("%s\n", buf);
-  write(fd1, buf, 74+inod_digits+strlen(input)+strlen(ctime(&filedata.st_mtime))+mtime_digits+size_digits);
-  */
-
-  /*
-  while((n = read(fd1, buf, BUFSIZE)) > 0)
+  if( (fd2 = open(output_path2, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU)) < 0)
     {
-      if(write(fd2, buf, n) < 0)
+      printf("Error creating instance in output folder for a directory\n");
+      exit(4);
+    }
+  write(fd2, buf, 106+inod_digits+strlen(input)+strlen(virtual_path)+strlen(ctime(&filedata.st_mtime))+mtime_digits+size_digits);
+
+  if (S_ISDIR(filedata.st_mode))
+    {
+      DIR *dir;
+      struct dirent *entry;
+      char create_dir_path[256];
+      
+      strcpy(create_dir_path, output_path);
+      strcat(create_dir_path, "/.");
+      strcat(create_dir_path, input);
+
+      if (stat(create_dir_path, &dirdata) != 0)
 	{
-	  printf("Error writing to file\n");
-	  exit(4);
+	  if (mkdir(create_dir_path, 0777) == -1)
+	    {
+	      printf("Error creating directory for accurate in the output file\n");
+	      exit(5);
+	    }
+	}
+      
+      if ((dir = opendir(virtual_path)) == NULL)
+	{
+	  printf("Error opening directory\n");
+	  exit(6);
+	}
+      while ((entry = readdir(dir)) != NULL)
+	{
+	  if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+	    {
+	      rezolvare(entry->d_name, virtual_path, create_dir_path);
+	    }
 	}
     }
-  */
-  
+
   close(fd1);
   close(fd2);
-
+  
   return;
 }
 
+/*
+void rezolvare2(char *input, char *output, char *path)
+{
+  int fd1, fd2;
+  char input_path[256], output_path[256], inode[256];
+  struct stat filedata;
+
+  strcpy(input_path, path);
+  strcat(input_path, "/");
+  strcat(input_path, input);
+
+  strcpy(output_path, path);
+  strcat(output_path, "/");
+  strcat(output_path, output);
+  
+  if ((fd1 = open(input_path, O_RDONLY)) < 0)
+    {
+      printf("Error opening input path\n");
+      exit(2);
+    }
+  fstat(fd1, &filedata);
+  snprintf(inode, "%ld", filedata.st_ino);
+
+  DIR *dir;
+  struct dirent *entry;
+  if ((dir = opendir(output_path)) == NULL)
+    {
+      printf("Error opening output path\n");
+      exit(3);
+    }
+  while ((entry = readdir(dir)) != NULL)
+    {
+      if (strcmp(entry->d_name, inode) == 0)
+	{
+	  // am gasit inode-ul fisierului in fisierul de iesire
+	}
+    }
+  
+  
+}
+*/
+
 int main(int argc, char *argv[])
 {
-  int i, status;
-  pid_t pid[11];   // vector de pid-uri ale proceselor create
+  int i, status, output_index;
+  char path[256], output_path[256];
+  pid_t pid[21];   // vector de pid-uri ale proceselor create
+  struct stat dirdata;
+
+  rename("/home/onusike/Desktop/Bananas", "/home/onusike/Desktop/Banana");
+  
   pid[0] = 1;
-  // printf("%d\n", argc);
+  for (i=0; i<=20; i++)
+    {
+      pid[i] = 1;
+    }
+  
   for (i=1; i<argc-1; i++)
     {
-      if (pid[i-1] != 0)   // pid[i-1] este pid-ul ultimului proces creat, daca nu e 0 ne aflam in procesul parinte
+      if (argv[i][0] == '-')
 	{
-	  if ( (pid[i]=fork()) < 0 )   // creare procesul fiu cu numarul i
+	  if (argv[i][1] == 'o')
 	    {
-	      perror("Eroare");
-	      exit(0);
-	    }
-	  if (pid[i] == 0)   // in procesul fiu se monitorizeaza modificarile asupra fisierului i transmis in linia de comanda
-	    {
-	      // cod fiu
-	      rezolvare(i, argv[i], argv[argc-1]);
-	      exit(1);
+	      output_index = i+1;
 	    }
 	}
     }
-  for (i=2; i<argc-1; i++)
+
+  strcpy(path, realpath(".", NULL));
+  strcpy(output_path, realpath(".", NULL));
+  strcat(output_path, "/");
+  strcat(output_path, argv[output_index]);
+
+  if (stat(output_path, &dirdata) != 0)
     {
-      waitpid(pid[i], &status, 0);   // waitpid pentru fiecare proces fiu
-      if(WIFEXITED(status))
-	printf("\nProcesul cu PID-ul %d s-a încheiat cu codul %d\n", pid[i], WEXITSTATUS(status));
-      else
-	printf("\nAnormalitate in cadrul procesului cu PID-ul %d\n", pid[i]);
+      if (mkdir(output_path, 0777) == -1)
+	{
+	  printf("Error creating the output directory\n");
+	  exit(1);
+	}
+    }
+  
+  for (i=1; i<argc; i++)
+    {
+      if (argv[i][0] != '-' && i != output_index)
+	{
+	  if (pid[i-1] != 0)   // pid[i-1] este pid-ul ultimului proces creat, daca nu e 0 ne aflam in procesul parinte
+	    {
+	      if ( (pid[i]=fork()) < 0 )   // creare procesul fiu cu numarul i
+		{
+		  printf("Error creating child process");
+		  exit(2);
+		}
+	      if (pid[i] == 0)   // in procesul fiu se monitorizeaza modificarile asupra fisierului i transmis in linia de comanda
+		{
+		  // cod fiu
+		  rezolvare(argv[i], path, output_path);
+		  exit(0);
+		}
+	    }
+	}
+    }
+  for (i=1; i<argc; i++)
+    {
+      if (argv[i][0] != '-' && i != output_index)
+	{
+	  waitpid(pid[i], &status, 0);   // waitpid pentru fiecare proces fiu
+	  if(WIFEXITED(status))
+	    printf("\nProcesul cu PID-ul %d s-a încheiat cu codul %d (%s)\n", pid[i], WEXITSTATUS(status), argv[i]);
+	  else
+	    printf("\nAnormalitate in cadrul procesului cu PID-ul %d (%s)\n", pid[i], argv[i]);
+
+	}
     }
   return 0;
 }
